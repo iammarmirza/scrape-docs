c9f8b887ea723b0b2812a6f73fb7861c40393967
Ammar Mirza <itsammarmirza@gmail.com>
2024-10-08 19:50:14 +0530

    Add guides

---
+++ guides/Enterprise Server/version-1/config.ts
@@ -0,0 +1,49 @@
+export default {
+  "settings": {
+    "name": "V1",
+    "slug": "v1",
+    "isDefault": true
+  },
+  "sidebar": [
+    {
+      "type": "page",
+      "path": "./enterprise-server-overview.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-requirements.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-installation.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-vcs-integrations.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-configuration.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-enterprise-upgrade-guide.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-control-panel.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-integration-jira-cloud.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-integration-slack.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./enterprise-server-troubleshooting.mdx"
+    }
+  ]
+};
---
+++ guides/Integrations/version-1/config.ts
@@ -0,0 +1,25 @@
+export default {
+  "settings": {
+    "name": "V1",
+    "slug": "v1",
+    "isDefault": true
+  },
+  "sidebar": [
+    {
+      "type": "page",
+      "path": "./integrations-slack.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./integrations-jira.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./integrations-vanta.mdx"
+    },
+    {
+      "type": "page",
+      "path": "./integrations-sso.mdx"
+    }
+  ]
+};
